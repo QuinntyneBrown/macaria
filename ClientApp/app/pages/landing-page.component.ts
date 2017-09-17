f4cd004742d30a0b830515b1769deb96b2f65571
Quinntyne Brown <quinntynebrown@gmail.com>
2017-09-16 21:10:15 -0400

    clean up auto saving algorithm

--- ClientApp/app/pages/landing-page.component.ts
+++ ClientApp/app/pages/landing-page.component.ts
@@ -9,6 +9,7 @@ import {EventHub} from "../shared/services/event-hub";
 import {ActivatedRoute} from "@angular/router";
 import {Subscription} from "rxjs/Subscription";
 import {FormGroup,FormControl,Validators} from "@angular/forms";
+import {Observable} from "rxjs/Observable";
 
 declare var moment: any;
 
@@ -24,13 +25,7 @@ export class LandingPageComponent {
         private _notesService: NotesService,
         private _elementRef: ElementRef,
         private _eventHub: EventHub
-    ) {
-        this.saveNote = this.saveNote.bind(this);
-    }
-
-    public ngOnInit() {
-        
-    }
+    ) { }
     
     public quillEditorFormControl: FormControl = new FormControl('');
     
@@ -50,45 +45,33 @@ export class LandingPageComponent {
 
         this._notesService.getByCurrentUser().subscribe(x => this.notes$.next(x.notes)); 
 
-        this._elementRef.nativeElement.querySelector("ce-quill-text-editor").addEventListener("keydown", this.saveNote);        
-    }
+        Observable
+            .fromEvent(this._elementRef.nativeElement.querySelector("ce-quill-text-editor"), "keyup")
+            .debounce(() => Observable.timer(300))
+            .map(() => {
+                const correlationId = this._correlationIdsList.newId();
 
-    public timeoutId: any = null;
-    public saving: boolean = false;
+                this._notesService.addOrUpdate({
+                    correlationId,
+                    note: {
+                        id: this.note$.value.id,
+                        title: this.note$.value.title,
+                        body: this.quillEditorFormControl.value
+                    },
+                }).subscribe();
 
-    public saveNote() {       
-        if (this.saving) return;
-
-        this.saving = true;
-
-        setTimeout(() => {
-            
-            const correlationId = this._correlationIdsList.newId();
-
-            this._notesService.addOrUpdate({
-                correlationId,
-                note: {
-                    id: this.note$.value.id,
-                    title: this.note$.value.title,
-                    body: this.quillEditorFormControl.value
-                },
+                this._eventHub.events.subscribe(x => {
+                    if (x.payload.correlationId == correlationId) {
+                        if (x.payload.entity)
+                            this.notes$.next(addOrUpdate({
+                                item: x.payload.entity,
+                                items: this.notes$.value
+                            }));
+                    }
+                });
             }).subscribe();
-
-            this._eventHub.events.subscribe(x => {                
-                if (x.payload.correlationId == correlationId) {
-                    
-                    if(x.payload.entity)
-                        this.notes$.next(addOrUpdate({
-                            item: x.payload.entity,
-                            items: this.notes$.value
-                        }));                                         
-                    this.saving = false;
-                }
-            });
-        }, 1000);
-        
     }
-
+    
     private _saveSubscription: Subscription;
 
     public notes$: BehaviorSubject<Array<Note>> = new BehaviorSubject([]);
